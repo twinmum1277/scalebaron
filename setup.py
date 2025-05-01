8fbf69138b47d189654f87ee525b73789b4fd9cb
twinmum1277 <tracy.punshon@dartmouth.edu>
2025-05-01 10:25:41 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+setup(
+    name='scalebaron',
+    version='1.0.0',
+    description='ScaleBarOn: A Python Tool for Scaling Multiple Elemental Maps',
+    author='Tracy Punshon',
+    author_email='tracy.punshon@dartmouth.edu',
+    license='MIT',
+    py_modules=['scalebaron'],  # or use packages=find_packages() if it's a directory
+    install_requires=[
+        'numpy',
+        'pandas',
+        'matplotlib',
+        'Pillow',
+        'openpyxl',
+        'scipy'
+    ],
+    entry_points={
+        'console_scripts': [
+            'scalebaron = scalebaron:main',
+        ]
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
+)
