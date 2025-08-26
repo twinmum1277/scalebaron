70374c91d5fd74f9351aad250b8c818127ce80da
twinmum1277 <tracy.punshon@dartmouth.edu>
2025-08-26 11:49:44 -0400

    Update muaddata.py

--- scalebaron/muaddata.py
+++ scalebaron/muaddata.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import os
 
-class MuadDataViewer:
+class MuadDataViewer:  
     def __init__(self, root):
         self.root = root
         self.root.title("Muad'Data - Elemental Map Viewer")
